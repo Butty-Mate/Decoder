d023df8c33ddf2f632636721ac7c043737765488
ButtyMate <118981147+Butty-Mate@users.noreply.github.com>
2025-03-06 14:05:26 +0100

    decoder_v1
    decoder_v1_test

---
+++ BM-dekoder/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace BM_dekoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip1 = new System.Windows.Forms.TextBox();
+            this.ip2 = new System.Windows.Forms.TextBox();
+            this.message = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_dekod = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.dataGridView_send = new System.Windows.Forms.DataGridView();
+            this.datagrid_ip1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datagrid_ip2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datagrid_message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datagrid_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dategrid_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_send)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ip1
+            // 
+            this.ip1.Location = new System.Drawing.Point(35, 32);
+            this.ip1.Name = "ip1";
+            this.ip1.Size = new System.Drawing.Size(100, 20);
+            this.ip1.TabIndex = 0;
+            // 
+            // ip2
+            // 
+            this.ip2.Location = new System.Drawing.Point(35, 67);
+            this.ip2.Name = "ip2";
+            this.ip2.Size = new System.Drawing.Size(100, 20);
+            this.ip2.TabIndex = 2;
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(36, 101);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(100, 20);
+            this.message.TabIndex = 3;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(166, 29);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 4;
+            this.btn_send.Text = "Küldés";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // btn_dekod
+            // 
+            this.btn_dekod.Location = new System.Drawing.Point(166, 65);
+            this.btn_dekod.Name = "btn_dekod";
+            this.btn_dekod.Size = new System.Drawing.Size(75, 23);
+            this.btn_dekod.TabIndex = 5;
+            this.btn_dekod.Text = "Dekódolás";
+            this.btn_dekod.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(166, 99);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 6;
+            this.btn_exit.Text = "Kilépés";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView_send
+            // 
+            this.dataGridView_send.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_send.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.datagrid_ip1,
+            this.datagrid_ip2,
+            this.datagrid_message,
+            this.datagrid_date,
+            this.dategrid_number});
+            this.dataGridView_send.Location = new System.Drawing.Point(12, 160);
+            this.dataGridView_send.Name = "dataGridView_send";
+            this.dataGridView_send.Size = new System.Drawing.Size(765, 208);
+            this.dataGridView_send.TabIndex = 7;
+            // 
+            // datagrid_ip1
+            // 
+            this.datagrid_ip1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.datagrid_ip1.HeaderText = "IP1";
+            this.datagrid_ip1.Name = "datagrid_ip1";
+            this.datagrid_ip1.ReadOnly = true;
+            // 
+            // datagrid_ip2
+            // 
+            this.datagrid_ip2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.datagrid_ip2.HeaderText = "IP2";
+            this.datagrid_ip2.Name = "datagrid_ip2";
+            this.datagrid_ip2.ReadOnly = true;
+            // 
+            // datagrid_message
+            // 
+            this.datagrid_message.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.datagrid_message.HeaderText = "Message";
+            this.datagrid_message.Name = "datagrid_message";
+            this.datagrid_message.ReadOnly = true;
+            // 
+            // datagrid_date
+            // 
+            this.datagrid_date.HeaderText = "Date";
+            this.datagrid_date.Name = "datagrid_date";
+            this.datagrid_date.ReadOnly = true;
+            // 
+            // dategrid_number
+            // 
+            this.dategrid_number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dategrid_number.HeaderText = "Number";
+            this.dategrid_number.Name = "dategrid_number";
+            this.dategrid_number.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_send);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_dekod);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.ip2);
+            this.Controls.Add(this.ip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_send)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ip1;
+        private System.Windows.Forms.TextBox ip2;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_dekod;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.DataGridView dataGridView_send;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_ip1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_ip2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_message;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dategrid_number;
+    }
+}
+
---
+++ BM-dekoder/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BM_dekoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const string uzenetKezdete = "xxx";
+            const string uzenetVege = ".-+.";
+            const string fileVege = ".-.-.+.+.";
+            const string elvalaszto = ".x.";
+
+            string inputFilePath = "messages.log";
+            string outputFilePath = "output.txt";
+
+            StringBuilder output = new StringBuilder();
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int lineNumber = 1;
+
+            foreach (string line in lines)
+            {
+                if (line.Contains(uzenetKezdete) && line.Contains(uzenetVege))
+                {
+                    string[] parts = line.Split(new string[] { elvalaszto }, StringSplitOptions.None);
+                    if (parts.Length >= 4)
+                    {
+                        string timestamp = parts[0].Substring(uzenetKezdete.Length);
+                        Debug.WriteLine($" hiba {timestamp}");
+                        timestamp = timestamp.Substring(timestamp.Length - 18);
+                        Debug.WriteLine($" jo {timestamp}");
+                        string message = parts[1];
+                        string ip1Hex = parts[2];
+                        string ip2Hex = parts[3].Substring(0, parts[3].IndexOf(uzenetVege));
+
+                        long ticks = long.Parse(timestamp);
+                        DateTime dateTime = new DateTime(ticks);
+
+
+                        output.AppendLine($"{lineNumber} - {dateTime} - {message} - {BinaryToIp(ip1Hex)} - {BinaryToIp(ip2Hex)}");
+                        lineNumber++;
+                    }
+                }
+            }
+
+            File.WriteAllText(outputFilePath, output.ToString());
+
+            /*
+            string[] adat = File.ReadAllLines("message.log");
+            int db = 0;
+            foreach (string i in adat)
+            { 
+            if (i == "x"){
+                db++;
+                    if (db == 3)
+                    {
+
+                    }
+            }
+            else db = 0;
+                
+            }
+            */
+
+        }
+        static string BinaryToIp(string binary)
+        {
+            string ipCim = "";
+
+            for (int i = 0; i < 32; i += 8)
+            {
+
+                int ipdeci = Convert.ToInt32(binary.Substring(i, 8), 2);
+                ipCim += ipdeci;
+
+                if (i < 24)
+                {
+                    ipCim += ".";
+                }
+            }
+
+            return ipCim;
+        }
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            /*
+            StreamWriter writer = new StreamWriter("message.txt"); 
+            //if (ip1.TextLength < 15 && ip1.Text < 7)
+            string Ip1Send = ip1.Text;
+            string Ip2Send = ip2.Text;
+            string MessageSend = message.Text;
+            string DateSend = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string ujSor = $"";
+            */
+        }
+    }
+}
