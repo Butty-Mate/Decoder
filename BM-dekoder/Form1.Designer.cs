89df07a6ae88ba827cb0cf935e17bbe24bce0000
ButtyMate00 <118981147+ButtyMate00@users.noreply.github.com>
2025-03-11 22:43:46 +0100

    update
    code,decode

--- BM-dekoder/Form1.Designer.cs
+++ BM-dekoder/Form1.Designer.cs
@@ -35,38 +35,36 @@
             this.btn_dekod = new System.Windows.Forms.Button();
             this.btn_exit = new System.Windows.Forms.Button();
             this.dataGridView_send = new System.Windows.Forms.DataGridView();
-            this.datagrid_ip1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.datagrid_ip2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.datagrid_message = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.datagrid_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dategrid_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IP1_label = new System.Windows.Forms.Label();
+            this.IP2_label = new System.Windows.Forms.Label();
+            this.M_label = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_send)).BeginInit();
             this.SuspendLayout();
             // 
             // ip1
             // 
-            this.ip1.Location = new System.Drawing.Point(35, 32);
+            this.ip1.Location = new System.Drawing.Point(77, 31);
             this.ip1.Name = "ip1";
             this.ip1.Size = new System.Drawing.Size(100, 20);
             this.ip1.TabIndex = 0;
             // 
             // ip2
             // 
-            this.ip2.Location = new System.Drawing.Point(35, 67);
+            this.ip2.Location = new System.Drawing.Point(77, 66);
             this.ip2.Name = "ip2";
             this.ip2.Size = new System.Drawing.Size(100, 20);
             this.ip2.TabIndex = 2;
             // 
             // message
             // 
-            this.message.Location = new System.Drawing.Point(36, 101);
+            this.message.Location = new System.Drawing.Point(78, 100);
             this.message.Name = "message";
             this.message.Size = new System.Drawing.Size(100, 20);
             this.message.TabIndex = 3;
             // 
             // btn_send
             // 
-            this.btn_send.Location = new System.Drawing.Point(166, 29);
+            this.btn_send.Location = new System.Drawing.Point(200, 27);
             this.btn_send.Name = "btn_send";
             this.btn_send.Size = new System.Drawing.Size(75, 23);
             this.btn_send.TabIndex = 4;
@@ -76,75 +74,70 @@
             // 
             // btn_dekod
             // 
-            this.btn_dekod.Location = new System.Drawing.Point(166, 65);
+            this.btn_dekod.Location = new System.Drawing.Point(200, 63);
             this.btn_dekod.Name = "btn_dekod";
             this.btn_dekod.Size = new System.Drawing.Size(75, 23);
             this.btn_dekod.TabIndex = 5;
             this.btn_dekod.Text = "Dekódolás";
             this.btn_dekod.UseVisualStyleBackColor = true;
+            this.btn_dekod.Click += new System.EventHandler(this.btn_dekod_Click);
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(166, 99);
+            this.btn_exit.Location = new System.Drawing.Point(200, 97);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(75, 23);
             this.btn_exit.TabIndex = 6;
             this.btn_exit.Text = "Kilépés";
             this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
             // 
             // dataGridView_send
             // 
             this.dataGridView_send.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_send.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.datagrid_ip1,
-            this.datagrid_ip2,
-            this.datagrid_message,
-            this.datagrid_date,
-            this.dategrid_number});
             this.dataGridView_send.Location = new System.Drawing.Point(12, 160);
             this.dataGridView_send.Name = "dataGridView_send";
             this.dataGridView_send.Size = new System.Drawing.Size(765, 208);
             this.dataGridView_send.TabIndex = 7;
             // 
-            // datagrid_ip1
+            // IP1_label
             // 
-            this.datagrid_ip1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.datagrid_ip1.HeaderText = "IP1";
-            this.datagrid_ip1.Name = "datagrid_ip1";
-            this.datagrid_ip1.ReadOnly = true;
+            this.IP1_label.AutoSize = true;
+            this.IP1_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.IP1_label.Location = new System.Drawing.Point(27, 38);
+            this.IP1_label.Name = "IP1_label";
+            this.IP1_label.Size = new System.Drawing.Size(23, 13);
+            this.IP1_label.TabIndex = 8;
+            this.IP1_label.Text = "IP1";
             // 
-            // datagrid_ip2
+            // IP2_label
             // 
-            this.datagrid_ip2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.datagrid_ip2.HeaderText = "IP2";
-            this.datagrid_ip2.Name = "datagrid_ip2";
-            this.datagrid_ip2.ReadOnly = true;
+            this.IP2_label.AutoSize = true;
+            this.IP2_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.IP2_label.Location = new System.Drawing.Point(27, 74);
+            this.IP2_label.Name = "IP2_label";
+            this.IP2_label.Size = new System.Drawing.Size(23, 13);
+            this.IP2_label.TabIndex = 9;
+            this.IP2_label.Text = "IP2";
             // 
-            // datagrid_message
+            // M_label
             // 
-            this.datagrid_message.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.datagrid_message.HeaderText = "Message";
-            this.datagrid_message.Name = "datagrid_message";
-            this.datagrid_message.ReadOnly = true;
-            // 
-            // datagrid_date
-            // 
-            this.datagrid_date.HeaderText = "Date";
-            this.datagrid_date.Name = "datagrid_date";
-            this.datagrid_date.ReadOnly = true;
-            // 
-            // dategrid_number
-            // 
-            this.dategrid_number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dategrid_number.HeaderText = "Number";
-            this.dategrid_number.Name = "dategrid_number";
-            this.dategrid_number.ReadOnly = true;
+            this.M_label.AutoSize = true;
+            this.M_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.M_label.Location = new System.Drawing.Point(27, 108);
+            this.M_label.Name = "M_label";
+            this.M_label.Size = new System.Drawing.Size(50, 13);
+            this.M_label.TabIndex = 10;
+            this.M_label.Text = "Message";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.M_label);
+            this.Controls.Add(this.IP2_label);
+            this.Controls.Add(this.IP1_label);
             this.Controls.Add(this.dataGridView_send);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.btn_dekod);
@@ -170,11 +163,9 @@
         private System.Windows.Forms.Button btn_dekod;
         private System.Windows.Forms.Button btn_exit;
         private System.Windows.Forms.DataGridView dataGridView_send;
-        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_ip1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_ip2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_message;
-        private System.Windows.Forms.DataGridViewTextBoxColumn datagrid_date;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dategrid_number;
+        private System.Windows.Forms.Label IP1_label;
+        private System.Windows.Forms.Label IP2_label;
+        private System.Windows.Forms.Label M_label;
     }
 }
 
--- BM-dekoder/Form1.cs
+++ BM-dekoder/Form1.cs
@@ -1,96 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Drawing;
-using System.Globalization;
 using System.IO;
-using System.IO.Ports;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace BM_dekoder
 {
     public partial class Form1 : Form
     {
+        const string UzenetKezdete = "xxx";
+        const string UzenetVege = ".-+.";
+        const string FileVege = ".-.-.+.+.";
+        const string Elvalaszto = ".x.";
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView_send.Visible = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            const string uzenetKezdete = "xxx";
-            const string uzenetVege = ".-+.";
-            const string fileVege = ".-.-.+.+.";
-            const string elvalaszto = ".x.";
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Sorszám");
+            tabla.Columns.Add("IP1");
+            tabla.Columns.Add("IP2");
+            tabla.Columns.Add("Üzenet");
+            tabla.Columns.Add("Időbélyeg");
 
-            string inputFilePath = "messages.log";
-            string outputFilePath = "output.txt";
+            dataGridView_send.DataSource = tabla;
+        }
 
-            StringBuilder output = new StringBuilder();
-            string[] lines = File.ReadAllLines(inputFilePath);
-            int lineNumber = 1;
+        private void DekodolEsMegjelenit()
+        {
+            // Fájl elérési útja
+            string fajlUt = "messages.log";
 
-            foreach (string line in lines)
+            // DataTable létrehozása a táblázatos megjelenítéshez
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Sorszám");
+            tabla.Columns.Add("IP1");
+            tabla.Columns.Add("IP2");
+            tabla.Columns.Add("Üzenet");
+            tabla.Columns.Add("Időbélyeg");
+
+            // Fájl beolvasása soronként
+            string[] sorok = File.ReadAllLines(fajlUt);
+            int sorszam = 1;
+
+            foreach (string sor in sorok)
             {
-                if (line.Contains(uzenetKezdete) && line.Contains(uzenetVege))
+                // Ellenőrizzük, hogy a sor tartalmazza-e az üzenet kezdetét és végét
+                if (sor.Contains(UzenetKezdete) && sor.Contains(UzenetVege))
                 {
-                    string[] parts = line.Split(new string[] { elvalaszto }, StringSplitOptions.None);
-                    if (parts.Length >= 4)
+                    // A sort részekre bontjuk az elválasztó alapján
+                    string[] reszek = sor.Split(new string[] { Elvalaszto }, StringSplitOptions.None);
+
+                    if (reszek.Length >= 4)
                     {
-                        string timestamp = parts[0].Substring(uzenetKezdete.Length);
-                        Debug.WriteLine($" hiba {timestamp}");
-                        timestamp = timestamp.Substring(timestamp.Length - 18);
-                        Debug.WriteLine($" jo {timestamp}");
-                        string message = parts[1];
-                        string ip1Hex = parts[2];
-                        string ip2Hex = parts[3].Substring(0, parts[3].IndexOf(uzenetVege));
+                        // Időbélyeg kinyerése és formázása
+                        string idoBelyeg = reszek[0].Substring(UzenetKezdete.Length);
+                        idoBelyeg = idoBelyeg.Substring(idoBelyeg.Length - 18);
+                        DateTime datumIdo = new DateTime(long.Parse(idoBelyeg));
 
-                        long ticks = long.Parse(timestamp);
-                        DateTime dateTime = new DateTime(ticks);
+                        // Üzenet és IP címek kinyerése
+                        string uzenet = reszek[1];
+                        string feladoIp = reszek[2];
+                        string cimzettIp = reszek[3].Substring(0, reszek[3].IndexOf(UzenetVege));
 
+                        // IP címek binárisból decimálisba konvertálása
+                        string feladoIpDec = BinarisbolIp(feladoIp);
+                        string cimzettIpDec = BinarisbolIp(cimzettIp);
 
-                        output.AppendLine($"{lineNumber} - {dateTime} - {message} - {BinaryToIp(ip1Hex)} - {BinaryToIp(ip2Hex)}");
-                        lineNumber++;
+                        // Ha a konverzió sikeres, akkor hozzáadjuk a táblázathoz
+                        if (!string.IsNullOrEmpty(feladoIpDec) && !string.IsNullOrEmpty(cimzettIpDec))
+                        {
+                            tabla.Rows.Add(sorszam, feladoIpDec, cimzettIpDec, uzenet, datumIdo);
+                            sorszam++;
+                        }
                     }
                 }
             }
 
-            File.WriteAllText(outputFilePath, output.ToString());
-
-            /*
-            string[] adat = File.ReadAllLines("message.log");
-            int db = 0;
-            foreach (string i in adat)
-            { 
-            if (i == "x"){
-                db++;
-                    if (db == 3)
-                    {
-
-                    }
-            }
-            else db = 0;
-                
-            }
-            */
-
+            // A DataGridView frissítése a dekódolt adatokkal
+            dataGridView_send.DataSource = tabla;
         }
-        static string BinaryToIp(string binary)
+
+        // Bináris IP cím átalakítása decimális formátumba
+        private string BinarisbolIp(string binarisIp)
         {
+            // Ellenőrizzük, hogy a bináris IP cím pontosan 32 karakter hosszú-e
+            if (binarisIp.Length != 32)
+            {
+                MessageBox.Show($"Hibás bináris IP cím hossz: {binarisIp.Length} karakter. Pontosan 32 karakternek kell lennie.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            // Ellenőrizzük, hogy a bináris IP cím csak 0 és 1 karaktereket tartalmaz-e
+            foreach (char c in binarisIp)
+            {
+                if (c != '0' && c != '1')
+                {
+                    MessageBox.Show($"Hibás karakter a bináris IP címben: {c}. Csak 0 és 1 karakterek megengedettek.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+
             string ipCim = "";
 
             for (int i = 0; i < 32; i += 8)
             {
-
-                int ipdeci = Convert.ToInt32(binary.Substring(i, 8), 2);
-                ipCim += ipdeci;
+                int decimális = Convert.ToInt32(binarisIp.Substring(i, 8), 2);
+                ipCim += decimális;
 
                 if (i < 24)
                 {
@@ -100,17 +121,78 @@ namespace BM_dekoder
 
             return ipCim;
         }
+
+        // Új üzenet hozzáadása a fájlhoz
         private void btn_send_Click(object sender, EventArgs e)
         {
-            /*
-            StreamWriter writer = new StreamWriter("message.txt"); 
-            //if (ip1.TextLength < 15 && ip1.Text < 7)
-            string Ip1Send = ip1.Text;
-            string Ip2Send = ip2.Text;
-            string MessageSend = message.Text;
-            string DateSend = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            string ujSor = $"";
-            */
+            string feladoIp = ip1.Text;
+            string cimzettIp = ip2.Text;
+            string uzenet = message.Text;
+            long idoBelyeg = DateTime.Now.Ticks;
+
+            // IP címek ellenőrzése és bináris formátumba konvertálása
+            string feladoIpBin = IpbolBinaris(feladoIp);
+            string cimzettIpBin = IpbolBinaris(cimzettIp);
+
+            if (string.IsNullOrEmpty(feladoIpBin) || string.IsNullOrEmpty(cimzettIpBin))
+            {
+                MessageBox.Show("Hibás IP cím formátum!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Az új sor formázása a kódolási szabályok szerint
+            string ujSor = $"xxx{idoBelyeg}.x.{uzenet}.x.{feladoIpBin}.x.{cimzettIpBin}.-+.";
+
+            // Az új sor hozzáadása a fájlhoz
+            File.AppendAllText("messages.log", ujSor + Environment.NewLine);
+
+            // Üzenet elküldve, mezők törlése
+            ip1.Text = "";
+            ip2.Text = "";
+            message.Text = "";
+
+            MessageBox.Show("Üzenet elküldve!", "Siker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Decimális IP cím átalakítása bináris formátumba
+        private string IpbolBinaris(string ip)
+        {
+            string[] reszek = ip.Split('.');
+            string binaris = "";
+
+            foreach (string resz in reszek)
+            {
+                // Ellenőrizzük, hogy az IP cím részei számok-e
+                if (!int.TryParse(resz, out int decimális))
+                {
+                    MessageBox.Show($"Hibás IP cím rész: {resz}. Csak számok megengedettek.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+
+                // Ellenőrizzük, hogy az IP cím részei 0 és 255 között vannak-e
+                if (decimális < 0 || decimális > 255)
+                {
+                    MessageBox.Show($"Hibás IP cím rész: {resz}. Az értéknek 0 és 255 között kell lennie.", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+
+                // Bináris formátumba konvertálás (8 biten)
+                binaris += Convert.ToString(decimális, 2).PadLeft(8, '0');
+            }
+
+            return binaris;
+        }
+
+        // Kilépés a programból
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Dekódolás gomb eseménykezelője
+        private void btn_dekod_Click(object sender, EventArgs e)
+        {
+            DekodolEsMegjelenit();
         }
     }
 }
